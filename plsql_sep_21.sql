3666d55bfb8ec6be12a9a6cf6dbcd961c6510a9a
sailohithkolli <sailohith.kolli31@gmail.com>
2024-09-22 13:08:49 -0400

    sql_test_1

--- plsql_sep_21.sql
+++ plsql_sep_21.sql
@@ -17,3 +17,24 @@ end loop;
     close c1;
 end;
 /
+
+select * from jobs;
+
+set serveroutput on;
+
+declare 
+    cursor c1 is select * from jobs;
+    v_job jobs%ROWTYPE;
+begin
+    open c1;
+loop
+    fetch c1 into v_job;
+exit when c1%notfound;
+dbms_output.put_line(v_job.job_id);
+dbms_output.put_line(v_job.job_title);
+dbms_output.put_line(v_job.min_salary);
+dbms_output.put_line(v_job.max_salary);
+end loop;
+    close c1;
+end;
+/
